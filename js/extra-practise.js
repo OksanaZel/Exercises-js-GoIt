c3b75cf238ffbc77938df774176d4882bae478e2
Oksana Zelenevskaya <exoticisl@gmail.com>
2021-05-12 22:42:40 +0300

    Добавляет задачу

---
+++ js/extra-practise.js
@@ -0,0 +1,21 @@
+// const promise = new Promise((resolve, reject) => {
+//     reject([{ name: 'Jhon'}, {name: 'Peter'}]);
+// })
+
+// promise
+//     .then(([resp, resp2]) => console.log(resp, resp2))
+//     .catch(err => console.log(err))
+//     .finally(()=> console.log('finally'));
+
+// setTimeout(() => {
+//     document.body.style.backgroundColor = "tomato"
+// }, 2000);
+
+// let intervalId = setInterval(() => {
+//     const seconds = new Date(Date.now()).getUTCSeconds();
+//     console.log(seconds);
+//     if (seconds < 40) {
+//         clearInterval(intervalId);
+//     }
+//  }, 1000);
+
